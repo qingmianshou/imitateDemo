7f846a7276808ed744ee334a5bc56f4fc778d48b
YangZhi <1059119903@qq.com>
2016-10-20 18:04:27 +0800

    first commit

---
+++ app/src/main/java/com/example/imitatedemo/SettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.imitatedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
